2a8f4a040e5501e47ff9cd84046b8e258f727c8a
Barbodmsd <barbodmasoudi@gmail.com>
2024-06-02 12:04:42 +0430

    end authSlice

--- front/src/Store/Slices/authSlice.js
+++ front/src/Store/Slices/authSlice.js
@@ -1,4 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit"
+
 const initialState={
     token:null,
     user:null,
 }
+// create autSlice
+const authSlice=createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        // write the actions for authSlice
+        sinIn:(state,action)=>{
+            state.token=action.payload.token
+            state.user=action.payload.user
+        },
+        signOut:(state)=>{
+            state.token=null
+            state.user=null
+        }
+    }
+})
+export const {signIn,signOut}=authSlice.actions
+export default authSlice.reducer
+// export the what we need
