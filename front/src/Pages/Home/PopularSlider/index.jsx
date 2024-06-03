85d746f156d4580ed59b5d893037abdca1d2c509
Barbodmsd <barbodmasoudi@gmail.com>
2024-06-03 14:54:27 +0430

    paste popular slider

---
+++ front/src/Pages/Home/PopularSlider/index.jsx
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import './PopularSlider.css';
+
+import { Scrollbar } from 'swiper/modules';
+
+export default function PopularSlider() {
+  return (
+    <>
+      <Swiper
+        scrollbar={{
+          hide: true,
+        }}
+        modules={[Scrollbar]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
