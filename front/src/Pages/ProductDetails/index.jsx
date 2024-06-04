a4076ef583f88ed32461ad190583532b03c6b52d
Barbodmsd <barbodmasoudi@gmail.com>
2024-06-04 20:33:46 +0430

    end atropos

--- front/src/Pages/ProductDetails/index.jsx
+++ front/src/Pages/ProductDetails/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import fetchData from '../../Utils/fetchData'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Skeleton, Stack, Typography } from '@mui/material'
 import 'atropos/atropos.css'
 import Atropos from 'atropos/react'
 export default function ProductDetails({ theme }) {
@@ -16,122 +16,88 @@ export default function ProductDetails({ theme }) {
   return (
     <>
       {/* product details */}
-      <Stack justifyContent={'center'} gap={'30px'} sx={{ p: '20px 70px' }}>
+      {
+        product ? <Stack justifyContent={'center'} gap={'30px'} sx={{ p: '20px 70px' }}>
+          {/* name */}
+          <Box><Typography color={'txt.two'} fontSize={'2rem'} fontWeight={'bolder'}>{product?.attributes?.name}</Typography></Box>
+          {/* description */}
+          <Box ><Typography sx={{ color: theme == 'light' ? '#4f4f4f' : 'txt.three' }}>{product?.attributes?.description}</Typography></Box>
+          {/* image atropos */}
+          <Stack justifyContent={'center'} direction={'center'} minHeight={600}>
+            <Stack component={Atropos} className={'atropos'}
+              sx={{
+                width: '700px',
+                height: '500px',
+              }}>
+              <Stack className={'atropos-scale'}>
+                <Stack className={'atropos-rotate'}>
+                  <Stack className={'atropos-inner'}>
+                    <img data-atropos-offset="-5" style={{ borderRadius: 15 }} src={import.meta.env.VITE_URL + product?.attributes?.image?.data[0]?.attributes?.url} width={'100%'} height={'100%'} />
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        bottom: "12%",
+                        left: "7%",
+                      }}
+                    >
+                      <Typography
+                        data-atropos-offset='0'
+                        data-atropos-opacity='0.2;0.9'
+                        sx={{
+                          color: "grey",
+                          fontSize: "1.5rem",
+                          fontWeight: "bolder",
+                        }}
+                      >
+                        LOREM
+                      </Typography>
+
+                    </Box>
+                    <Box sx={{
+                      position: "absolute",
+                      bottom: "8%",
+                      left: "7%",
+                    }}>
+                      <Typography
+                        data-atropos-offset='8'
+                        sx={{
+                          color: "grey",
+                          fontSize: "1.5rem",
+                          fontWeight: "bolder",
+                        }}
+                      >
+                        IPSUM PLACEHOLDER
+                      </Typography>
+                    </Box>
+                  </Stack>
+                </Stack>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack> :
+        // skeleton
+        <Stack justifyContent={'center'} gap={'30px'} sx={{ p: '30px 70px' }}>
         {/* name */}
-        <Box><Typography color={'txt.two'} fontSize={'2rem'} fontWeight={'bolder'}>{product?.attributes?.name}</Typography></Box>
+        <Skeleton variant={'rounded'} width={'150px'} height={'40px'} animation={'wave'}/>
         {/* description */}
-        <Box ><Typography sx={{ color: theme == 'light' ? '#4f4f4f' : 'txt.three' }}>{product?.attributes?.description}</Typography></Box>
+        <Stack gap={'5px'}>
+        <Skeleton variant={'rounded'} width={'100%'} height={'40px'} animation={'wave'}/>
+        <Skeleton variant={'rounded'} width={'100%'} height={'40px'} animation={'wave'}/>
+        <Skeleton variant={'rounded'} width={'100%'} height={'40px'} animation={'wave'}/>
+        </Stack>
         {/* image atropos */}
-         {/* <Stack justifyContent={'center'} width={'100%'} height={'600px'} direction={'row'} > */}
-         <Stack
-          className={"atropos"}
-          component={Atropos}
-          justifyContent={'center'}
-          direction={'row'}
+        <Stack justifyContent={'center'} direction={'center'} minHeight={600}>
+          <Stack 
           sx={{
-            width: 500,
-            height: 500,
-            // p:'20px',
-            overflow:'hidden'
-          }}
-        >
- <Box className={"atropos-scale"}>
-          <Box className={"atropos-rotate"}>
-            <Box className={"atropos-inner"}>
-              <img
-                src={import.meta.env.VITE_URL + product?.attributes?.image?.data[0]?.attributes?.url}
-                data-atropos-offset="-5"
-                width="100%"
-                height="100%"
-              />
-              {/* <Box
-                sx={{
-                  position: "absolute",
-                  bottom: "11%",
-                  left: "7%",
-                }}
-              >
-                <Typography
-                data-atropos-offset='0' data-atropos-opacity="0.8;0.2"
-                  sx={{
-                    color: "txt.three",
-                    fontSize: "1.5rem",
-                    fontWeight: "bolder",
-                  }}
-                >
-                  CAR 
-                </Typography>
-               
-              </Box>
-              <Box sx={{
-                  position: "absolute",
-                  bottom: "7%",
-                  left: "7%",
-                }}>
-              <Typography
-                data-atropos-offset='8'
-                  sx={{
-                    color: "txt.three",
-                    fontSize: "1.5rem",
-                    fontWeight: "bolder",
-                  }}
-                >
-                   DRIVER
-                </Typography>
-              </Box> */}
-            </Box>
-          </Box>
-        </Box> 
-        {/* </Stack> */}
+            width: '700px',
+            height: '500px',
+          }}>
+             <Skeleton variant={'rounded'} width={'100%'} height={'100%'} animation={'wave'}/>
+          </Stack>
         </Stack>
       </Stack>
+      }
     </>
   )
 }
-//  <Box className={"atropos-scale"}>
-//           <Box className={"atropos-rotate"}>
-//             <Box className={"atropos-inner"}>
-//               <img
-//                 // src={"src/assets/dog.webp"}
-//                 data-atropos-offset="-5"
-//                 width="100%"
-//                 height="100%"
-//               />
-//               <Box
-//                 sx={{
-//                   position: "absolute",
-//                   bottom: "11%",
-//                   left: "7%",
-//                 }}
-//               >
-//                 <Typography
-//                 data-atropos-offset='0' data-atropos-opacity="0.8;0.2"
-//                   sx={{
-//                     color: "white",
-//                     fontSize: "1.5rem",
-//                     fontWeight: "bolder",
-//                   }}
-//                 >
-//                   CAR 
-//                 </Typography>
-               
-//               </Box>
-//               <Box sx={{
-//                   position: "absolute",
-//                   bottom: "7%",
-//                   left: "7%",
-//                 }}>
-//               <Typography
-//                 data-atropos-offset='8'
-//                   sx={{
-//                     color: "white",
-//                     fontSize: "1.5rem",
-//                     fontWeight: "bolder",
-//                   }}
-//                 >
-//                    DRIVER
-//                 </Typography>
-//               </Box>
-//             </Box>
-//           </Box>
-//         </Box> 
+
