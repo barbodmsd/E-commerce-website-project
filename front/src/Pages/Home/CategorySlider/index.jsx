effb7e30eccc3e1210a28e185e843211fa2744a5
Barbodmsd <barbodmasoudi@gmail.com>
2024-06-04 11:50:51 +0430

    write slider

---
+++ front/src/Pages/Home/CategorySlider/index.jsx
@@ -0,0 +1,68 @@
+import { Box, Stack, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Mousewheel, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import fetchData from '../../../Utils/fetchData';
+import './Category.css';
+
+// card for category slider 
+export const CatCard = ({ img, name, id, description }) => {
+  return <Link to={`/products/${id}/${name}`}>
+    <Stack sx={{
+      width: '100%',
+      height: '100%',
+      borderRadius: '20px',
+      overFlow: 'hidden',
+      position: 'relative',
+    }}>
+      <Box sx={{
+        position: 'absolute',
+        left: '5%',
+        top: '20%'
+      }}><Typography>{description.slice(0, 100)}</Typography></Box>
+      <Box><img width='100%' height='100%' src={img} alt={name} /></Box>
+    </Stack>
+  </Link>
+}
+
+export default function CategorySlider() {
+  const [cat, setCat] = useState()
+  useEffect(() => {
+    (async () => {
+      const res = await fetchData('categories?populate=*')
+      setCat(res)
+    })()
+  }, [])
+  const items = cat?.map((e, index) => <SwiperSlide key={index}>
+    <CatCard name={e.attributes?.name} id={e.id} description={e.attributes?.description} img={import.meta.env.VITE_URL + e?.attributes?.image?.data[0]?.attributes?.url} />
+  </SwiperSlide>)
+  return (
+    <>
+      <Box sx={{
+        width: '100%',
+        height: '600px',
+        p: '50px'
+      }}>
+        <Swiper
+          direction={'vertical'}
+          slidesPerView={1}
+          spaceBetween={30}
+          mousewheel={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Mousewheel, Pagination]}
+          className="category-slider"
+        >
+
+          {
+            items
+          }
+        </Swiper>
+      </Box>
+    </>
+  );
+}
--- front/src/Pages/Home/index.jsx
+++ front/src/Pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BannerSlider from './BannerSlider'
 import BgAttachment from './BgAttahment'
 import SliderProducts from '../../Components/Slider'
+import CategorySlider from './CategorySlider'
 
 export default function Home({ theme }) {
   return (
@@ -12,6 +13,8 @@ export default function Home({ theme }) {
       <SliderProducts title={'Popular'} theme={theme} route={'/products/all-products/all-categories'} model={'products'} field={'popular'} operator={'$eq'} value={true} />
       {/* bg attachment */}
       <BgAttachment />
+      {/* category slider */}
+      <CategorySlider/>
     </>
   )
 }
