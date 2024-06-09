903b7601487b4cfc5bd87229b6e3d3148bf88479
Barbodmsd <barbodmasoudi@gmail.com>
2024-06-09 18:20:01 +0430

    start auth

--- front/src/Pages/auth/index.jsx
+++ front/src/Pages/auth/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Auth() {
+  const [pageType,setPageType]=useState()
   return (
     <div>Auth</div>
   )
